8a3ff25dc77684b34a785aee9c8eb311fe4a6b9a
LouisM-create <louis.moser@endress.com>
2025-11-04 13:51:49 +0000

    Ich_weitergemacht_habe

--- muri_logics/muri_logics/logic_action_server_init.py
+++ muri_logics/muri_logics/logic_action_server_init.py
@@ -1,36 +1,47 @@
 from enum import Enum
 from  muri_logics.logic_interface import LogicInterface, Out
 
+
 class InitStates(Enum):
     INIT = 0
     IDLE = 1
     FAILED = 2
     SUCCESS = 3
-    RUN = 4 #EVTL. Sinfolle namen für RUN und MOVE
-    MOVE = 5
+    RAEDY = 4 
+    INITMOVE = 5
+
 
 class InitOut(Out):
     def __init__(): 
-        self.__values = None #TODO
+        self.__values = None
         pass #TODO
 
     @property
     def values(self):
         """Get the value."""
-        pass #TODO
+        return self.__values
 
     @values.setter
-    def values(self, val):
+    def values(self, data):
         """Set the value."""
-        pass #TODO
+        lvx, lvy, avz, ta = data
 
-    def getState(self):
-        """Retrieve the current state."""
-        pass #TODO
+        if lvx is not None:
+            self.__values['linear_velocity_x'] = lvx
+        
+        if lvy is not None:
+            self.__values['lineat_velocity_y'] = lvy
 
+        if avz is not None:
+            self.__values['angular_velocity_z'] = avz
+
+        if ta is not None:
+            self.__values['turned_angle'] = ta
+
+       
     def resetOut(self):
         """Reset the output to its initial state."""
-        pass #TODO
+        self.__values = None
     
     def outValid(self):
         """Check if the output is valid."""
@@ -65,7 +76,6 @@ class InitLogic(LogicInterface):
         self.__positionX = 0.0
         self.__positionY = 0.0
         self.__positionTheta = 0.0
-        self.__camaeraData = None
         self.__output.resetOut()
         self.__state = InitStates.IDLE
 
@@ -76,12 +86,30 @@ class InitLogic(LogicInterface):
         self.__positionTheta = t
 
 
-    def setCameraData(self, data):
+    def setCameraData(self, pToMid, pToMidPrev, pHeight, pHeightPrev, picWidth):
         """Sets the Data of the actual Position of the Robot, for the Processing Locig"""
-        self.__camaeraData = data
+        self.__pixelToMid = pToMid
+        self.__picelToMidPrev = pToMidPrev
+        self.__pixelHeight = pHeight
+        self.__pixelHeightPrev = pHeightPrev
+        self.__pictureWidth = picWidth
     
-    def calculate():
-        pass #TODO
+    def calculate(self):
+        
+        PIXELTOLLERANCE = 5
+        angularVelocityZ = 0
+
+        if self.__pixelToMid > 0 and abs(self.__pixelToMid) > PIXELTOLLERANCE:
+            angularVelocityZ = 1
+
+        elif self.__pixelToMid < 0 and abs(self.__pixelToMid) > PIXELTOLLERANCE:
+            angularVelocityZ = -1
+        
+        else:
+            angularVelocityZ = 0
+        
+        return angularVelocityZ
+
 
     
     def state_machine(self):
@@ -90,6 +118,7 @@ class InitLogic(LogicInterface):
         match self.__state:
 
             case InitStates.INIT:
+                self.__.output.values = (0.0, 0.0, 0.0, 0.0)
                 self.__positionX = 0.0
                 self.__positionY = 0.0
                 self.__positionTheta = 0.0
@@ -99,11 +128,17 @@ class InitLogic(LogicInterface):
             case InitStates.IDLE:
                 pass
 
-            case InitStates.RUN:
-                self.__state = InitStates.MOVE
+            case InitStates.RAEDY:
+                self.__state = InitStates.INITMOVE
 
-            case InitStates.MOVE:
-                self.calculate()
+            case InitStates.INITMOVE:
+                avz, ta = self.calculate()
+                self.__output.values = (None, None, avz, ta)
+                if avz = 0:
+                    self.__state = InitStates.SUCCESS
+                
+                
+
 
             case InitStates.FAILED:
                 pass #TODO
