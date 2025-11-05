b9a4995e5015de59eaececd219ce7d977b3a51ab
ibinsandig <you@example.com>
2025-11-05 12:51:46 +0100

    erstes halbwegs funktionierendes CV

--- misc/fuck_around_and_find_out/test_ scripts/old_version/box.py
+++ misc/fuck_around_and_find_out/test_ scripts/old_version/box.py
@@ -1,18 +1,36 @@
 import cv2 as cv
 import numpy as np
 
-img= cv.imread('data/final möglich.png')
+img = cv.imread('data/final möglich.png')
 
 img_hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
 
 # Maskenerstellung
+#lower_green = np.array([ 40,  50,  50])
+#upper_green = np.array([ 80, 255, 255])
+#mask_green = cv.inRange(img_hsv, lower_green, upper_green)
+
+
 lower_red = cv.inRange(img_hsv, np.array([  0, 50, 50]), np.array([ 10,255,255]))
 upper_red = cv.inRange(img_hsv, np.array([170, 50, 50]), np.array([180,255,255]))
 mask_red = lower_red | upper_red
 
 # Konturen finden
+#contours_green, _ = cv.findContours(mask_green, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE)
 contours_red, _ = cv.findContours(mask_red, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE)
+
+#print(f"Grüne Konturen gefunden: {len(contours_green)}")
 print(f"Rote Konturen gefunden: {len(contours_red)}")
+
+
+#if len(contours_green) > 0:
+    #largest_green = max(contours_green, key=cv.contourArea)
+    
+    #x_g, y_g, w_g, h_g = cv.boundingRect(largest_green)
+    
+    #print(f"Grüner Balken gefunden: ")
+    #print(f"Position: x={x_g}, y={y_g}")
+    #print(f"Größe: width={w_g}, height={h_g}")
 
 if len(contours_red) > 0:
     largest_red = max(contours_red, key=cv.contourArea)
@@ -26,10 +44,10 @@ if len(contours_red) > 0:
 
 center_x = x_r + w_r // 2 
 center_y = y_r + h_r // 2
+
 print(f"Mitte des roten Markers ({center_x}, {center_y})")
 
 picture_height, picture_width = img.shape[:2]
 pixel_to_mid = x_r - (picture_width)
-print(f"pixel_to_mid: {pixel_to_mid}")
 
-print(f"Höhe des roten Vierecks: {h_r}")
+print(f"pixel_to_mid: {pixel_to_mid}")
