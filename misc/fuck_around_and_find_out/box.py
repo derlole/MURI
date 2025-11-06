9fdc16bcc71f42c3f5b52a2995c00a7a1cf5d8d4
ibinsandig <lbraun0203@gmail.com>
2025-11-06 11:33:58 +0100

    test

--- misc/fuck_around_and_find_out/box.py
+++ misc/fuck_around_and_find_out/box.py
@@ -3,33 +3,37 @@ import numpy as np
 
 img= cv.imread('data/final möglich.png')
 
+
 img_hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
 
 # Maskenerstellung
-lower_red = cv.inRange(img_hsv, np.array([  0, 50, 50]), np.array([ 10,255,255]))
-upper_red = cv.inRange(img_hsv, np.array([170, 50, 50]), np.array([180,255,255]))
-mask_red = lower_red | upper_red
+lower_green = np.array([35, 50, 50])      # untere Grenze
+upper_green = np.array([85, 255, 255])    # obere Grenze
 
+mask_green = cv.inRange(img_hsv, lower_green, upper_green)
 # Konturen finden
-contours_red, _ = cv.findContours(mask_red, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE)
-print(f"Rote Konturen gefunden: {len(contours_red)}")
 
-if len(contours_red) > 0:
-    largest_red = max(contours_red, key=cv.contourArea)
+contours, hierarchy = cv.findContours(mask_green, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE)
 
-    x_r, y_r, w_r, h_r = cv.boundingRect(largest_red)
-    
-    print(f"Rotes Quadrat gefunden: ")
-    print(f"Position: x={x_r}, y={y_r}")
-    print(f"Größe: width={w_r}, height={h_r}")
+print(f"Grüne Konturen gefunden: {len(contours)}")
 
+if len(contours) > 0:
+        largest_green = max(contours, key=cv.contourArea)
+        x_r, y_r, w_r, h_r = cv.boundingRect(largest_green)
+        rectangle_center_x = x_r + w_r // 2
+        rectangle_center_y = y_r + h_r // 2
+        
+        print(f"Günes Rechteck gefunden: ")
+        print(f"Position: x={x_r}, y={y_r}")
+        print(f"Größe: width={w_r}, height={h_r}")
+        print(f"Mitte des Bereichs ({rectangle_center_x}, {rectangle_center_y})")
 
-center_x = x_r + w_r // 2 
-center_y = y_r + h_r // 2
-print(f"Mitte des roten Markers ({center_x}, {center_y})")
+        picture_height, picture_width = frame.shape[:2]
+        picture_mid_x = picture_width // 2
+        pixel_to_mid_x = rectangle_center_x - picture_mid_x
+        print(f"pixel_to_mid: {pixel_to_mid_x}")
 
-picture_height, picture_width = img.shape[:2]
-pixel_to_mid = x_r - (picture_width)
-print(f"pixel_to_mid: {pixel_to_mid}")
+        print(f"Höhe des grünen Vierecks: {h_r}")
 
-print(f"Höhe des roten Vierecks: {h_r}")
+else:
+    print("Kein Rechteck gefunden...")
--- misc/fuck_around_and_find_out/rechteck_live_tracking.py
+++ misc/fuck_around_and_find_out/rechteck_live_tracking.py
@@ -13,36 +13,30 @@ while True:
 
     lower_green = np.array([35, 50, 50])      # untere Grenze
     upper_green = np.array([85, 255, 255])    # obere Grenze
-
-    mask_red = cv.inRange(img_hsv, lower_green, upper_green)
-
-    # Maskenerstellung
-    #lower_red = cv.inRange(img_hsv, np.array([  0, 50, 50]), np.array([ 10,255,255]))
-    #upper_red = cv.inRange(img_hsv, np.array([170, 50, 50]), np.array([180,255,255]))
-    #mask_red = lower_red | upper_red
+    mask_green = cv.inRange(img_hsv, lower_green, upper_green)
 
     # Konturen finden
 
-    contours, hierarchy = cv.findContours(mask_red, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE)
-    print(f"Rote Konturen gefunden: {len(contours)}")
+    contours, hierarchy = cv.findContours(mask_green, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE)
+    print(f"Grüne Konturen gefunden: {len(contours)}")
 
     if len(contours) > 0:
-        largest_red = max(contours, key=cv.contourArea)
-        x_r, y_r, w_r, h_r = cv.boundingRect(largest_red)
+        largest_green = max(contours, key=cv.contourArea)
+        x_r, y_r, w_r, h_r = cv.boundingRect(largest_green)
         rectangle_center_x = x_r + w_r // 2
         rectangle_center_y = y_r + h_r // 2
         
-        print(f"Rotes Quadrat gefunden: ")
+        print(f"Grünes Quadrat gefunden: ")
         print(f"Position: x={x_r}, y={y_r}")
         print(f"Größe: width={w_r}, height={h_r}")
-        print(f"Mitte des roten Markers ({rectangle_center_x}, {rectangle_center_y})")
+        print(f"Mitte des Bereichs ({rectangle_center_x}, {rectangle_center_y})")
 
         picture_height, picture_width = frame.shape[:2]
         picture_mid_x = picture_width // 2
         pixel_to_mid_x = rectangle_center_x - picture_mid_x
         print(f"pixel_to_mid: {pixel_to_mid_x}")
 
-        print(f"Höhe des roten Vierecks: {h_r}")
+        print(f"Höhe des grünen Vierecks: {h_r}")
 
         cv.rectangle(frame, (x_r, y_r), (x_r + w_r, y_r + h_r), (0, 255, 0), 2)
         cv.circle(frame, (rectangle_center_x, rectangle_center_y), 4, (255, 0, 0), -1)
