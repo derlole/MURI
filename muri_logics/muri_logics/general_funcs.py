5afd63698c8d693633962d669d8264696b44f644
CrazyTurtle <m.louis-toni@gmx.de>
2025-11-17 12:11:09 +0100

    Now_Better

--- muri_logics/muri_logics/general_funcs.py
+++ muri_logics/muri_logics/general_funcs.py
@@ -12,16 +12,14 @@ def quaternion_to_yaw(q):
     return yaw
 
 
-def angular_velocity_regulationy(angel_to_Mid):
-    '''regulates the angular velocity based on the angular offset to the center'''
-    MAXANGLEVELOSETY = 0.1
-    kp = 1.5
 
-    angel_Vel = kp * angel_to_Mid
+def p_regulator(error, kp, max_output):
+    '''Proportional controller with saturation limits'''
+    output = kp * error
 
-    if angel_Vel > MAXANGLEVELOSETY:
-        angel_Vel = MAXANGLEVELOSETY
-    elif angel_Vel < MAXANGLEVELOSETY:
-        angel_Vel = -MAXANGLEVELOSETY
+    if output > max_output:
+        output = max_output
+    elif output < -max_output:
+        output = -max_output
     
-    return angel_Vel
+    return output
