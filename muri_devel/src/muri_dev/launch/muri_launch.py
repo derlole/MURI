b1f5126000b8e40de74df0600faf69be91f0996c
derlole <benjaaimnpekkler@gmail.com>
2025-11-18 20:30:46 +0100

    launch

--- muri_devel/src/muri_dev/launch/muri_launch.py
+++ muri_devel/src/muri_dev/launch/muri_launch.py
@@ -4,16 +4,6 @@ from launch.actions import ExecuteProcess
 
 def generate_launch_description():
     return LaunchDescription([
-        # Node camera_read_out on raspi 
-        # ExecuteProcess(
-        #     cmd=[
-        #         'sshpass', '-p', 'ipek2023',
-        #         'ssh', '-o', 'StrictHostKeyChecking=no',
-        #         'ubuntu@10.42.0.1',
-        #         'source /home/ubuntu/muri_team/muri_robot/install/setup.bash && ros2 run vision_proccessing camera_readout'
-        #     ],
-        #     shell=True
-        # ),
         Node(
             package='muri_dev',
             executable='muri_action_server_drive',
@@ -42,6 +32,12 @@ def generate_launch_description():
             package='vision_proccessing',
             executable='image_data_processing',
             name='image_data_processing',
+            output='screen'
+        ),
+        Node(            
+            package='vision_proccessing',
+            executable='camera_readout',
+            name='camera_readout',
             output='screen'
         ),
     ])
