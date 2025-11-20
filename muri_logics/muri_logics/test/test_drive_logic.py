f451471f07410b07e69aa8d61f7b23fd3bd3c46a
CrazyTurtle <m.louis-toni@gmx.de>
2025-11-20 12:30:34 +0100

    Unit_Test_anfang

---
+++ muri_logics/muri_logics/test/test_drive_logic.py
@@ -0,0 +1,10 @@
+import unittest
+import math
+from types import SimpleNamespace
+from unittest.mock import patch
+
+from logic_action_server_drive import DriveLogic, DriveStates, Constants
+
+
+class TestDriveLogic(unittest.TestCase):
+    pass #TODO
---
+++ muri_logics/muri_logics/test/test_turn_logic.py
@@ -0,0 +1,10 @@
+import unittest
+import math
+from types import SimpleNamespace
+from unittest.mock import patch
+
+from logic_action_server_turn import TurnLogic, TurnStates, Constants
+
+
+class TestTurnLogic(unittest.TestCase):
+    pass #TODO
